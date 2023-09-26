587c60ea6a013fd390400bd8ce9dd6780aef0316
qxqxia <qxqinxia@outlook.com>
2023-09-26 16:06:00 +0200

    auth with prisma

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module ({
-    imports: [AuthModule],
+    imports: [PrismaModule],
     controllers: [AuthController],
     providers: [AuthService]
 })
