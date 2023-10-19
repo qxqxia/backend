418256f636d55779701a1876fda8dd20d1a41f87
qxqxia <qxqinxia@outlook.com>
2023-10-19 14:20:09 +0200

    argon hash password not work

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
+import * as argon from 'argon2';
 // import { User, Bookmark} from '@prisma/client';
 
 @Injectable()
@@ -10,7 +11,18 @@ export class AuthService {
         return { msg: 'I have signed in'};
     }
 
-    signup(dto: AuthDto) {
-        return { msg: 'I have signed up'};
+    async signup(dto: AuthDto) {
+        // generate the password hash
+        const hash = await argon.hash(dto.password);
+        // save the new user in the db
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            },
+        });
+        // return the saved user
+        return user;
+        // return { msg: 'I have signed up'};
     }
 }
