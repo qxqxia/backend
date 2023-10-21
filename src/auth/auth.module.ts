4a156712776a16581ace08af3dd25798a0e06dc3
qxia <qxqinxia@outlook.com>
2023-10-21 14:24:12 +0200

    jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+//import { JwtStrategy } from './strategy';
 
 @Module ({
     imports: [JwtModule.register({})],
     // imports: [PrismaModule],
     controllers: [AuthController],
-    providers: [AuthService]
+    providers: [AuthService],
 })
 export class AuthModule {}
