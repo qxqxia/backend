d7c05544ae2e30f2946ae3c4af0a9489c483d382
qxia <qxqinxia@outlook.com>
2023-10-23 11:24:11 +0200

    no idea

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,12 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from "../src/app.module";
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { after } from 'node:test';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('App e2e', () => {
     let app: INestApplication;
+    let prisma: PrismaService;
+
     beforeAll(async () => {
       const moduleRef =
         await Test.createTestingModule
@@ -12,13 +14,16 @@ describe('App e2e', () => {
     imports: [AppModule],
   }).compile(); 
 
-  app = moduleRef.createNestApplication();
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
-  await app.init();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+    await app.init();
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
   });
 
   afterAll(() => {
